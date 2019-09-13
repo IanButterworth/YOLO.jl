7c5a3efc3393499a94324013721730457dbc1549
Ian <i.r.butterworth@gmail.com>
2019-09-13 14:24:04 -0400

    limit dataset during tests, given VOC download time too long

--- src/datasets.jl
+++ src/datasets.jl
@@ -1,13 +1,14 @@
 """
     download_dataset(;name="all")
 
-Download supported datasets.
+Download supported datasets. If folder exists, deletes first.
 """
-function download_dataset(;name="all")
+function download_dataset(name::String = "all")
     !isdir(datasets_dir) && mkdir(datasets_dir)
     if name == "voc" || name == "all"
-        voc_dir = joinpath(datasets_dir,"voc")
-        !isdir(voc_dir) && mkdir(voc_dir)
+        voc_dir = joinpath(datasets_dir, "voc")
+        isdir(voc_dir) && rm(voc_dir, force = true, recursive = true)
+        mkdir(voc_dir)
         tmploc = download("https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar")
         run(`tar xf $tmploc -C $(voc_dir)`)
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,13 @@ using YOLO, Test
 
 datadir = joinpath(dirname(dirname(pathof(YOLO))),"data")
 
-@info "Downloading datasets"
-!isdir(joinpath(datadir,"datasets","voc")) && download_dataset("voc")
-
-@testset "Example datasets have been downloaded" begin
-    datasetsdir = joinpath(datadir,"datasets")
-    @test isdir(joinpath(datasetsdir,"voc","VOCdevkit","VOC2007"))
-end
+# @info "Downloading VOC dataset for testing"
+# !isdir(joinpath(datadir,"datasets","voc")) && YOLO.download_dataset("voc")
+#
+# @testset "Example datasets have been downloaded" begin
+#     datasetsdir = joinpath(datadir,"datasets")
+#     @test isdir(joinpath(datasetsdir,"voc","VOCdevkit","VOC2007"))
+# end
 
 @testset "Example pretrained weights files have been downloaded" begin
     pretraineddir = joinpath(datadir,"pretrained")
