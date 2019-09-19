9721c037b5e1df1f55df242009f9099fef28544a
Ian <i.r.butterworth@gmail.com>
2019-09-19 12:09:40 -0400

    update voc to tar.gz and location to self-released

--- src/datasets.jl
+++ src/datasets.jl
@@ -11,16 +11,15 @@ Download supported datasets. If folder exists, deletes first.
 """
 function download_dataset(name::String = "all")
     !isdir(datasets_dir) && mkdir(datasets_dir)
-    if any(name .== ["voc", "VOC", "all"])
+    if any(name .== ["voc2007","VOC2007","voc", "VOC", "all"])
         voc_dir = joinpath(datasets_dir, "voc")
         voc_root = joinpath(voc_dir, "VOCdevkit")
         isdir(voc_root) && rm(voc_root, force = true, recursive = true)
         @info "Downloading dataset..."
-        tmploc = joinpath(voc_dir,"VOCtrainval_06-Nov-2007.tar")
-        download("https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar",tmploc)
+        tmploc = joinpath(voc_dir,"VOCtrainval_06-Nov-2007.tar.gz")
+        download("https://github.com/ianshmean/YOLO.jl/releases/download/VOC2007/VOCtrainval_06-Nov-2007.tar.gz",tmploc)
         @info "Extracting..."
         BinaryProvider.unpack(tmploc, voc_dir)
         rm(tmploc, force = true)
-        #run(`tar xf $tmploc -C $voc_dir`)
     end
 end
