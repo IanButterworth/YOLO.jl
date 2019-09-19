957108ddcbc0141723eea570a77110be4dcf0216
Ian Butterworth <i.r.butterworth@gmail.com>
2019-09-19 16:57:06 -0400

    make example image random

--- examples/loadRunVisualize.jl
+++ examples/loadRunVisualize.jl
@@ -3,12 +3,12 @@ using Makie, YOLO, BenchmarkTools
 #First time only (downloads 5011 images & labels!)
 YOLO.download_dataset("voc2007")
 
-settings = YOLO.pretrained.v2_tiny_voc.load(minibatch_size=1)
+settings = YOLO.pretrained.v2_tiny_voc.load(minibatch_size=1) #Process one image at a time
 model = YOLO.v2_tiny.load(settings)
 YOLO.loadWeights!(model, settings)
 
 voc = YOLO.datasets.VOC.populate()
-vocloaded = YOLO.load(voc, settings, indexes = [100])
+vocloaded = YOLO.load(voc, settings, indexes = [rand(1:5011)]) #Load a single random image
 
 #Run the model
 res = model(vocloaded.imstack_mat);
