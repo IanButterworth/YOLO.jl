3a7955f1df6379c8e21be6a0d396fddf53edf115
iuliancioarca <iulian.cioarca@gmail.com>
2019-11-19 01:56:40 +0200

    Add files via upload

--- src/models/v2/loadweights.jl
+++ src/models/v2/loadweights.jl
@@ -42,43 +42,60 @@ end
 
 Loads layers' weights from given weights file path.
 """
-function loadWeights!(model::v2Chain, settings::Settings)
+function loadWeights!(model::Chain_layers, settings::Settings, nr_constants = 4)
     outlength = settings.cell_bboxes * (5 + settings.num_classes)
 
-    open(settings.weights_filepath, "r") do io
-        readconstants!(io)
+    io = open(settings.weights_filepath, "r") #do io
+        readconstants!(io, nr_constants)
         #First Conv layer
-        loadconv!(model.layers[1], io, 3, 3, 3, 16)
+        loadconv!(model.layers[1].layers[1], io, 3, 3, 3, 32)#25
         #Second Conv layer
-        loadconv!(model.layers[3], io, 3, 3, 16, 32)
+        loadconv!(model.layers[1].layers[3], io, 3, 3, 32, 64)#37
         #Third Conv layer
-        loadconv!(model.layers[5], io, 3, 3, 32, 64)
+        loadconv!(model.layers[1].layers[5], io, 3, 3, 64, 128)#49
         #4th Conv layer
-        loadconv!(model.layers[7], io, 3, 3, 64, 128)
+        loadconv!(model.layers[1].layers[6], io, 1, 1, 128, 64)#57
         #5th Conv layer
-        loadconv!(model.layers[9], io, 3, 3, 128, 256)
+        loadconv!(model.layers[1].layers[7], io, 3, 3, 64, 128)#65
         #6th Conv layer
-        loadconv!(model.layers[11], io, 3, 3, 256, 512)
-        #YoloPad
-        model.layers[13].w[1, 1, 1, 1] = 1
-        #7th Conv layer
-        loadconv!(model.layers[14], io, 3, 3, 512, 1024)
-        #8th Conv layer
-        loadconv!(model.layers[15], io, 3, 3, 1024, 1024)
-        #last layer
-        read!(io, model.layers[16].b)
-        toRead = Array{Float32}(undef, 1024 * outlength)
-        read!(io, toRead)
-        toRead = reshape(toRead, 1, 1, 1024, outlength)
-        model.layers[16].w = permutedims(toRead, [2, 1, 3, 4])
-        model.layers[16].w = flipkernel(model.layers[16].w)
-
+        loadconv!(model.layers[1].layers[9], io, 3, 3, 128, 256)#77
+		#7th Conv layer
+        loadconv!(model.layers[1].layers[10], io, 1, 1, 256, 128)#85
+		#8th Conv layer
+        loadconv!(model.layers[1].layers[11], io, 3, 3, 128, 256)#93
+		#9th Conv layer
+        loadconv!(model.layers[1].layers[13], io, 3, 3, 256, 512)#105
+		#10th Conv layer
+        loadconv!(model.layers[1].layers[14], io, 1, 1, 512, 256)#113
+		#11th Conv layer
+        loadconv!(model.layers[1].layers[15], io, 3, 3, 256, 512)#121
+		#12th Conv layer
+        loadconv!(model.layers[1].layers[16], io, 1, 1, 512, 256)#129
+		#13th Conv layer
+        loadconv!(model.layers[1].layers[17], io, 3, 3, 256, 512)#137
+		#14th Conv layer
+        loadconv!(model.layers[2].layers[2], io, 3, 3, 512, 1024)#149
+		#15th Conv layer
+        loadconv!(model.layers[2].layers[3], io, 1, 1, 1024, 512)#157
+		#16th Conv layer
+        loadconv!(model.layers[2].layers[4], io, 3, 3, 512, 1024)#165
+		#17th Conv layer
+        loadconv!(model.layers[2].layers[5], io, 1, 1, 1024, 512)#173
+		#18th Conv layer
+        loadconv!(model.layers[2].layers[6], io, 3, 3, 512, 1024)#181
+		#19th Conv layer
+        loadconv!(model.layers[2].layers[7], io, 3, 3, 1024, 1024)#192
+		#20th Conv layer
+        loadconv!(model.layers[2].layers[8], io, 3, 3, 1024, 1024)#200
+		#21st Conv layer
+        loadconv!(model.layers[3].layers[1], io, 1, 1, 512, 64)#211
+		#22nd Conv layer
+        loadconv!(model.layers[4].layers[1], io, 3, 3, 1280, 1024)#225
+		#23rd Conv layer
+        loadconv_last!(model.layers[4].layers[2], io, 1, 1, 1024, 125)#233
         if GPU >= 0
-            model.layers[16].w = KnetArray(model.layers[16].w)
-            model.layers[16].b = KnetArray(model.layers[16].b)
-            model.layers[13].w = KnetArray(model.layers[13].w)
-        end
-    end
+          end
+    # end
 end
 
 """
@@ -105,15 +122,82 @@ function loadconv!(c, io::IOStream, d1, d2, d3, d4)
         c.b = KnetArray(c.b)
     end
 end
-
+function loadconv_last!(c, io::IOStream, d1, d2, d3, d4)
+    read!(io, c.b)
+    toRead = Array{Float32}(undef, d4 * d3 * d2 * d1)
+    read!(io, toRead)
+    toRead = reshape(toRead, d1, d2, d3, d4)
+    c.w = permutedims(toRead, [2, 1, 3, 4])
+    c.w = flipkernel(c.w)
+    if GPU >= 0
+        c.w = KnetArray(c.w)
+        c.b = KnetArray(c.b)
+    end
+end
 """
     readconstants!(io)
 
 Read constant and unnecessary numbers from the io stream
 """
-function readconstants!(io::IOStream)
-    major = read(io, Int32)
-    minor = read(io, Int32)
-    revision = read(io, Int32)
-    iseen = read(io, Int32)
+function readconstants!(io::IOStream, nr_constants = 4)
+    # major = read(io, Int32)
+    # minor = read(io, Int32)
+    # revision = read(io, Int32)
+    # iseen = read(io, Int32)
+	# dummy = read(io, Float32)
+	for i = 1:nr_constants
+		read(io, Float32)
+	end
 end
+
+## initial
+# io = open(settings.weights_filepath, "r") #do io
+# 	readconstants!(io)
+# 	#First Conv layer
+# 	loadconv!(model.layers[1], io, 3, 3, 3, 32)#25
+# 	#Second Conv layer
+# 	loadconv!(model.layers[3], io, 3, 3, 32, 64)#37
+# 	#Third Conv layer
+# 	loadconv!(model.layers[5], io, 3, 3, 64, 128)#49
+# 	#4th Conv layer
+# 	loadconv!(model.layers[6], io, 1, 1, 128, 64)#57
+# 	#5th Conv layer
+# 	loadconv!(model.layers[7], io, 3, 3, 64, 128)#65
+# 	#6th Conv layer
+# 	loadconv!(model.layers[9], io, 3, 3, 128, 256)#77
+# 	#7th Conv layer
+# 	loadconv!(model.layers[10], io, 1, 1, 256, 128)#85
+# 	#8th Conv layer
+# 	loadconv!(model.layers[11], io, 3, 3, 128, 256)#93
+# 	#9th Conv layer
+# 	loadconv!(model.layers[13], io, 3, 3, 256, 512)#105
+# 	#10th Conv layer
+# 	loadconv!(model.layers[14], io, 1, 1, 512, 256)#113
+# 	#11th Conv layer
+# 	loadconv!(model.layers[15], io, 3, 3, 256, 512)#121
+# 	#12th Conv layer
+# 	loadconv!(model.layers[16], io, 1, 1, 512, 256)#129
+# 	#13th Conv layer
+# 	loadconv!(model.layers[17], io, 3, 3, 256, 512)#137
+# 	#14th Conv layer
+# 	loadconv!(model.layers[19], io, 3, 3, 512, 1024)#149
+# 	#15th Conv layer
+# 	loadconv!(model.layers[20], io, 1, 1, 1024, 512)#157
+# 	#16th Conv layer
+# 	loadconv!(model.layers[21], io, 3, 3, 512, 1024)#165
+# 	#17th Conv layer
+# 	loadconv!(model.layers[22], io, 1, 1, 1024, 512)#173
+# 	#18th Conv layer
+# 	loadconv!(model.layers[23], io, 3, 3, 512, 1024)#181
+# 	#19th Conv layer
+# 	loadconv!(model.layers[24], io, 3, 3, 1024, 1024)#192
+# 	#20th Conv layer
+# 	loadconv!(model.layers[25], io, 3, 3, 1024, 1024)#200
+# 	#21st Conv layer
+# 	loadconv!(model.layers[26], io, 1, 1, 512, 64)#211
+# 	#22nd Conv layer
+# 	loadconv!(model.layers[27], io, 3, 3, 1280, 1024)#225
+# 	#23rd Conv layer
+# 	loadconv_last!(model.layers[28], io, 1, 1, 1024, 125)#233
+# 	if GPU >= 0
+# 	  end
--- src/models/v2/v2.jl
+++ src/models/v2/v2.jl
@@ -53,6 +53,24 @@ end; x)
 (c::Conv)(x) =
     c.f.(conv4(c.w, x; stride = c.stride, padding = c.padding) .+ c.b) #convolutional layer function
 
+# Define a custom chain of layers:
+struct Chain_layers; layers; Chain_layers(args...)=new(args); end
+function (c::Chain_layers)(img)
+    res_1_17   = c.layers[1](img)
+    res_18_25  = c.layers[2](res_1_17)
+	res_26_27  = c.layers[3](res_1_17)
+    res_28     = reorg(res_26_27, 2)
+    res_29     = concat(res_28, res_18_25)
+    res_30_31  = c.layers[4](res_29)
+end
+(c::Chain_layers)(x,y) = nll(c(x),y)
+
+concat(x,y) = (res = cat(x,y,dims=3))
+function reorg(x,stride)
+    d1,d2,nr_filt,nr_batch = size(x)
+    reshape(x,div(d1,stride),div(d2,stride),nr_filt*(stride^2),nr_batch)
+end
+
 #leaky function
 function leaky(x)
     return max(Float32(0.1) * x, x)
@@ -84,43 +102,85 @@ The symmetric padding part is implemented in the pooling function just before Yo
 """
 function load(sets::Settings)
     # comments are line numbers corresponding
-    # with https://github.com/pjreddie/darknet/blob/master/cfg/yolov2.cfg
-return v2Chain(
-    Conv(3, 3, 3, 32, 1, 1, leaky), #25
-    Pool(2, 2, 0), #33
-    Conv(3, 3, 32, 64, 1, 1, leaky), #37
-    Pool(2, 2, 0), #45
-    Conv(3, 3, 64, 128, 1, 1, leaky), #49
-    Conv(1, 1, 128, 64, 1, 1, leaky), #57
-    Conv(3, 3, 64, 128, 1, 1, leaky), #65
-    Pool(2, 2, 0), #73
-    Conv(3, 3, 128, 256, 1, 1, leaky), #77
-    Conv(1, 1, 256, 128, 1, 1, leaky), #85
-    Conv(3, 3, 128, 256, 1, 1, leaky), #93
-    Pool(2, 2, 0), #101
-    Conv(3, 3, 256, 512, 1, 1, leaky), #105
-    Conv(1, 1, 512, 256, 1, 1, leaky), #113
-    Conv(3, 3, 256, 512, 1, 1, leaky), #121
-    Conv(1, 1, 512, 256, 1, 1, leaky), #129
-    Conv(3, 3, 256, 512, 1, 1, leaky), #137
-    Pool(2, 2, 0), #145
-    Conv(3, 3, 512, 1024, 1, 1, leaky), #149
-    Conv(1, 1, 1024, 512, 1, 1, leaky), #157
-    Conv(3, 3, 512, 1024, 1, 1, leaky), #165
-    Conv(3, 3, 512, 1024, 1, 1, leaky), #173
-    Conv(3, 3, 512, 1024, 1, 1, leaky), #181
-    ######
-    Conv(3, 3, 512, 1024, 1, 1, leaky), #192
-    Conv(3, 3, 512, 1024, 1, 1, leaky), #200
-    #TODO: [route] #208 https://github.com/pjreddie/darknet/blob/61c9d02ec461e30d55762ec7669d6a1d3c356fb2/cfg/yolov2.cfg#L208
-    Conv(1, 1, 128, 64, 1, 1, leaky), #211
-    #TODO: [reorg] #219
-    #TODO: [route] #222
-    Conv(3, 3, 512, 1024, 1, 1, leaky), #225
-    Conv(1, 1, 1024, sets.cell_bboxes * (5 + sets.num_classes), 1, 0, identity), #233
-)
+    # with https://github.com/pjreddie/darknet/blob/master/cfg/yolov2-voc.cfg
+c1_17 = v2Chain(
+    Conv(3, 3, 3, 32, 1, 1, leaky), #25 l1
+    Pool(2, 2, 0), #33 l2
+    Conv(3, 3, 32, 64, 1, 1, leaky), #37 l3
+    Pool(2, 2, 0), #45 l4
+    Conv(3, 3, 64, 128, 1, 1, leaky), #49 l5
+    Conv(1, 1, 128, 64, 1, 0, leaky), #57 l6
+    Conv(3, 3, 64, 128, 1, 1, leaky), #65 l7
+    Pool(2, 2, 0), #73 l8
+    Conv(3, 3, 128, 256, 1, 1, leaky), #77 l9
+    Conv(1, 1, 256, 128, 1, 0, leaky), #85 l10
+    Conv(3, 3, 128, 256, 1, 1, leaky), #93 l11
+    Pool(2, 2, 0), #101 l12
+    Conv(3, 3, 256, 512, 1, 1, leaky), #105 l13
+    Conv(1, 1, 512, 256, 1, 0, leaky), #113 l14
+    Conv(3, 3, 256, 512, 1, 1, leaky), #121 l15
+    Conv(1, 1, 512, 256, 1, 0, leaky), #129 l16
+    Conv(3, 3, 256, 512, 1, 1, leaky) #137 l17
+    )
+c18_24 = v2Chain(
+	Pool(2, 2, 0), #145 l18
+	Conv(3, 3, 512, 1024, 1, 1, leaky), #149 l19
+	Conv(1, 1, 1024, 512, 1, 0, leaky), #157 l20
+	Conv(3, 3, 512, 1024, 1, 1, leaky), #165 l21
+	Conv(1, 1, 1024, 512, 1, 0, leaky), #173 l22
+	Conv(3, 3, 512, 1024, 1, 1, leaky), #181 l23
+	######
+	Conv(3, 3, 1024, 1024, 1, 1, leaky), #192 l24
+	Conv(3, 3, 1024, 1024, 1, 1, leaky) #200 l25
+	)
+c26_27 = v2Chain(
+	Conv(1, 1, 512, 64, 1, 0, leaky)
+	)
+
+c30_31 = v2Chain(
+	Conv(3, 3,1280 , 1024, 1, 1, leaky), #225 l39
+	Conv(1, 1, 1024, sets.cell_bboxes * (5 + sets.num_classes), 1, 0, identity), #233 l31
+	)
+
+return Chain_layers(c1_17, c18_24, c26_27, c30_31)
+
 end
 
 include(joinpath(@__DIR__, "loadweights.jl"))
 
 end #module
+# ## initial
+# return v2Chain(
+#     Conv(3, 3, 3, 32, 1, 1, leaky), #25 l1
+#     Pool(2, 2, 0), #33 l2
+#     Conv(3, 3, 32, 64, 1, 1, leaky), #37 l3
+#     Pool(2, 2, 0), #45 l4
+#     Conv(3, 3, 64, 128, 1, 1, leaky), #49 l5
+#     Conv(1, 1, 128, 64, 1, 1, leaky), #57 l6
+#     Conv(3, 3, 64, 128, 1, 1, leaky), #65 l7
+#     Pool(2, 2, 0), #73 l8
+#     Conv(3, 3, 128, 256, 1, 1, leaky), #77 l9
+#     Conv(1, 1, 256, 128, 1, 1, leaky), #85 l10
+#     Conv(3, 3, 128, 256, 1, 1, leaky), #93 l11
+#     Pool(2, 2, 0), #101 l12
+#     Conv(3, 3, 256, 512, 1, 1, leaky), #105 l13
+#     Conv(1, 1, 512, 256, 1, 1, leaky), #113 l14
+#     Conv(3, 3, 256, 512, 1, 1, leaky), #121 l15
+#     Conv(1, 1, 512, 256, 1, 1, leaky), #129 l16
+#     Conv(3, 3, 256, 512, 1, 1, leaky), #137 l17
+#     Pool(2, 2, 0), #145 l18
+#     Conv(3, 3, 512, 1024, 1, 1, leaky), #149 l19
+#     Conv(1, 1, 1024, 512, 1, 1, leaky), #157 l20
+#     Conv(3, 3, 512, 1024, 1, 1, leaky), #165 l21
+#     Conv(1, 1, 1024, 512, 1, 1, leaky), #173 l22
+#     Conv(3, 3, 512, 1024, 1, 1, leaky), #181 l23
+#     ######
+#     Conv(3, 3, 1024, 1024, 1, 1, leaky), #192 l24
+#     Conv(3, 3, 1024, 1024, 1, 1, leaky), #200 l25
+#     #TODO: [route] #208 https://github.com/pjreddie/darknet/blob/61c9d02ec461e30d55762ec7669d6a1d3c356fb2/cfg/yolov2.cfg#L208 l26
+#     Conv(1, 1, 512, 64, 1, 1, leaky), #211 l27
+#     #TODO: [reorg] #219 l28
+#     #TODO: [route] #222 l29
+#     Conv(3, 3,1280 , 1024, 1, 1, leaky), #225 l39
+#     Conv(1, 1, 1024, sets.cell_bboxes * (5 + sets.num_classes), 1, 0, identity), #233 l31
+# )
