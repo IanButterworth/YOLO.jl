85a9945cc157edc91d0c8c9f79ed6b69e8db113f
Ian <i.r.butterworth@gmail.com>
2019-11-21 20:45:50 -0500

    remove benchmarktools

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,7 @@
 using YOLO
-using Test, BenchmarkTools, DataFrames
+using Test, DataFrames
 using FileIO, ImageCore, ImageTransformations
 
-df = DataFrame(model=String[], load=Bool[], load_time=Float64[], run=Bool[], forwardpass_time=Float64[], objects_detected=Int64[])
 prepareimage(img, w, h) = gpu(reshape(permutedims(Float32.(channelview(imresize(img, w, h))[1:3,:,:]), [3,2,1]), h, w, 3, 1))
 
 pkgdir = dirname(@__DIR__)
@@ -18,7 +17,8 @@ modelsAndWeights = [
                 ]
 IMG = load(joinpath(@__DIR__,"images","dog-cycle-car.png"))
 
-for (i, modelAndWeights) in pairs(modelsAndWeights[2:3])
+df = DataFrame(model=String[], load=Bool[], load_time=Float64[], run=Bool[], forwardpass_time=Float64[], objects_detected=Int64[])
+for (i, modelAndWeights) in pairs(modelsAndWeights)
     model = modelAndWeights[1]
     weights = modelAndWeights[2]
     expectedresults = modelAndWeights[3]
@@ -28,20 +28,21 @@ for (i, modelAndWeights) in pairs(modelsAndWeights[2:3])
         cfg_file = joinpath(pkgdir, "models", "$(model).cfg")
         weights_file = YOLO.getArtifact(weights)
 
-        t = @elapsed begin
+        t_load = @elapsed begin
             yolomod = YOLO.Yolo(cfg_file, weights_file, 1, silent=true)
         end
         new_df[1, :load] = true
-        new_df[1, :load_time] = t
+        new_df[1, :load_time] = t_load
 
         IMG_for_model = prepareimage(IMG, yolomod.cfg[:width], yolomod.cfg[:height])
         res = yolomod(IMG_for_model)
-        t = @belapsed $yolomod($IMG_for_model);
+        t_run = @elapsed yolomod(IMG_for_model);
         new_df[1, :run] = true
-        new_df[1, :forwardpass_time] = t
+        new_df[1, :forwardpass_time] = t_run
         new_df[1, :objects_detected] = size(res,2)
         @test size(res,2) == expectedresults
         append!(df, new_df)
+        @info "Loaded in $(round(t_load, digits=2)) seconds. Ran in $(round(t_run, digits=2)) seconds."
     end
     GC.gc()
 end
